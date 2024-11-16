4a292c3b34e0880eb63532c6456dd4b205a64462
Ruy Goes <ruygoes05@gmail.com>
2024-11-16 10:20:34 -0300

    criação dos bds

---
+++ Biblioteca/bd/Criação do BD.sql
@@ -0,0 +1,37 @@
+Create table favoritos (
+id int primary key auto_increment not null,
+codigo varchar(20) unique
+);
+drop table favoritos;
+select * from destaques;
+INSERT INTO destaques (codigo) VALUES
+('5veyEAAAQBAJ'),
+('tsNbpFw3PdMC'),
+('_VIoEQAAQBAJ'),
+('NEm4DgAAQBAJ'),
+('qDYQCwAAQBAJ'),
+('d7s8EAAAQBAJ'),
+('R9QnDwAAQBAJ'),
+('Ye_6DwAAQBAJ'),
+('-DgQCwAAQBAJ'),
+('THsEEAAAQBAJ');
+# romances
+CREATE TABLE romance (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    codigo VARCHAR(50)
+);
+INSERT INTO romance (codigo)
+VALUES
+('3wohEJsBaI0C'),
+('d7s8EAAAQBAJ'),
+('ChZXdaeMOgAC'),
+('nOgl4G7ayDcC'),
+('10qIEAAAQBAJ'),
+('qLptEAAAQBAJ'),
+('UGIC7N0Op2MC'),
+('vC84EAAAQBAJ'),
+('QiRuEAAAQBAJ'),
+('luSmDwAAQBAJ');
+
+
+
